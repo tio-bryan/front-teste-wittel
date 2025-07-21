5297152ad940621a48176cb32a7fc06bc480c618
Bryan <bryan.pesca@gmail.com>
2025-07-20 23:05:37 -0300

    removed DB configs

--- front_teste_wittel/settings.py
+++ front_teste_wittel/settings.py
@@ -75,17 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'front_teste_wittel.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
